1211387464272a41f72c3854675c6a0038f4e42a
sakshisharma753 <sakshisharmaofficial13@gmail.com>
2026-05-23 19:58:59 +0530

    added --license flag and improve --help CLI layout

--- file_manager.cpp
+++ file_manager.cpp
@@ -1842,8 +1842,28 @@ int main(int argc, char* argv[]) {
         std::cout << "Options:" << std::endl;
         std::cout << "  -v, --version         Show version information" << std::endl;
         std::cout << "  -h, --help            Show this help message" << std::endl;
+        std::cout << "  --license             Display the license information" << std::endl; // Improved help output
         std::cout << "  --cwd-file <file>     Write the final working directory to <file> on exit"
                   << std::endl;
+        return 0;
+      } else if (arg == "--license") { // New license flag handling
+        std::cout << "MIT License\n\n";
+        std::cout << "Copyright (c) 2026 Bimbok\n\n";
+        std::cout << "Permission is hereby granted, free of charge, to any person obtaining a copy\n";
+        std::cout << "of this software and associated documentation files (the \"Software\"), to deal\n";
+        std::cout << "in the Software without restriction, including without limitation the rights\n";
+        std::cout << "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n";
+        std::cout << "copies of the Software, and to permit persons to whom the Software is\n";
+        std::cout << "furnished to do so, subject to the following conditions:\n\n";
+        std::cout << "The above copyright notice and this permission notice shall be included in all\n";
+        std::cout << "copies or substantial portions of the Software.\n\n";
+        std::cout << "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n";
+        std::cout << "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n";
+        std::cout << "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n";
+        std::cout << "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n";
+        std::cout << "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n";
+        std::cout << "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n";
+        std::cout << "SOFTWARE." << std::endl;
         return 0;
       } else if (arg == "--cwd-file" && i + 1 < argc) {
         cwdFileArg = argv[++i];
